878fce0b5acc11f23362c6deb7c04582a05d35be
Mihanchos <shofer2007@gmail.com>
2024-08-13 15:31:11 +0500

    New func BPs work finding

--- CalculationValveRods/InputFromDBandUSER.py
+++ CalculationValveRods/InputFromDBandUSER.py
@@ -4,4 +4,11 @@ from CalculationValveRods.DATABASE.ClapansByTurbin import find_BP_clapans
 turbin_name = input("Введите название турбины: ")
 BPs, BPs_infos = find_BP_clapans(turbin_name)
 
-needed_BPs = input(f"Найдено {len(BPs)} чертеж(а/ей): ")
+print(f"Найдено {len(BPs)} чертеж(а/ей): {" , ".join(BPs)}")
+needed_BPs = input("Введите интересующий вас чертеж: ")
+
+if needed_BPs in BPs:
+    print("CONGRATULATIONS", BPs_infos[BPs.index(needed_BPs)], sep="\n")
+else:
+    print("\nДанный чертеж не найден среди чертежей выбранной турбины."
+          "\nПожалуйста пересмотрите список найденных чертежей.")
