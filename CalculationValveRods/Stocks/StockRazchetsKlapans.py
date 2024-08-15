8bc854c2cd436c08709a6249d73105546920d3b6
Mihanchos <shofer2007@gmail.com>
2024-08-15 13:46:39 +0500

    Stocks Adaptation UPDv2

--- CalculationValveRods/Stocks/StockRazchetsKlapans.py
+++ CalculationValveRods/Stocks/StockRazchetsKlapans.py
@@ -2,9 +2,11 @@ import numpy as np
 from scipy.interpolate import interp1d
 from math import pi
 from models import ClapanModel
-from WSAProperties import steamPT, steamPH # Correct need
+from WSAProperties import steamPT, steamPH  # Correct need
 
-''' Functions PART '''
+'''
+Functions PART (additional + steam/air)
+'''
 
 
 # Function to calculate friction resistance coefficient
@@ -48,33 +50,53 @@ def air_calc(A, B):
     return {0: RO, 1: V, 2: Din_vis, 3: Kin_vis}[B]
 
 
-''' Variables PART '''
+# Вспомогательная функция для нахождения G пара, или же воздуха для последней части
+def G_find(last_part, ALFA, P_first, P_second, v):
+    if not last_part:
+        # Определение параметров воздуха последнего участка
+        G = max(0.001, ALFA * S * ((P_first ** 2 - P_second ** 2) / (P_first * v)) ** 0.5 * 3.6)
+    else:
+        # Определение параметров пара от первого до предпоследнего участка
+        G = ALFA * S * ((P_first ** 2 - P_second ** 2) / (P_first * v)) ** 0.5 * 3.6
+    return G
 
-# # Input parameters - constant
-# Delta_part1, W_part1 = None, None
-# Delta_part2, W_part2 = None, None
-# Delta_part3, W_part3 = None, None
-#
-# # Output parameters - constant
-# _Delta_part1, _W_part1 = None, None
-# _Delta_part2, _W_part2 = None, None
-# _Delta_part3, _W_part3 = None, None
+
+# Функция для определения параметров пара участка
+def part_props_detection(P_first, P_second, v, din_vis, len_part, last_part=False, W=50):
+    if P_first == P_second:
+        P_first += 0.03
+    kin_vis = v * din_vis
+    delta_speed = 1
+    while not (-0.001 < delta_speed < 0.001):
+        Re = (W * 2 * delta_clearance) / kin_vis
+        ALFA = 1 / (1 + KSI + (0.5 * lambda_calc(Re) * len_part) / delta_clearance) ** 0.5
+        G = G_find(last_part, ALFA, P_first, P_second, v)
+        delta_speed = W - v * G / (3.6 * S)
+        if delta_speed <= 0.001:
+            W += max(0.001, W)
+        elif delta_speed >= 0.001:
+            W -= max(0.001, W * 0.9)
+    Re = (W * 2 * delta_clearance) / kin_vis
+    ALFA = 1 / (1 + KSI + (0.5 * lambda_calc(Re) * len_part) / delta_clearance) ** 0.5
+    G = G_find(last_part, ALFA, P_first, P_second, v)
+    return G
+
+
+'''
+Variables PART
+
+General geometric parameters
+radius_rounding                                       - Radius of inlet rounding or chamfer size
+delta_clearance                                       - Radial clearance
+diameter_stock                                        - Stem diameter
+len_part1, len_part2, len_part3, len_part4, len_part5 - Lengths of each section (to meters)
+'''
 
 temperature_start_valve = ClapanModel.T0
 pressure_start_valve = ClapanModel.P0
 enthalpy_steam = steamPT(pressure_start_valve * 98066.5, temperature_start_valve, 3) / 4186.8
 pressure_deaerator = ClapanModel.Pout_1
 pressure_ejector = ClapanModel.Pout_2
-
-# General geometric parameters
-# if ClapanModel.Calc_type == 0:
-#     r = ClapanModel.r_inlet / 1000  # Radius of inlet rounding or chamfer size
-#     delt = ClapanModel.delt_b_s / 1000  # Radial clearance
-#     d = ClapanModel.D_stem / 1000  # Stem diameter
-#     L1 = ClapanModel.L1_b / 1000  # Length of section 1
-#     L2 = ClapanModel.L2_b / 1000  # Length of section 2
-#     L3 = ClapanModel.L3_b / 1000  # Length of section 3
-# else:
 
 radius_rounding = ClapanModel.r_inlet_DB / 1000 if ClapanModel.r_inlet_DB is not None else None
 delta_clearance = ClapanModel.delt_DB / 1000 if ClapanModel.delt_DB is not None else None
@@ -86,105 +108,56 @@ len_part4 = ClapanModel.L4_DB / 1000 if ClapanModel.L4_DB is not None else None
 len_part5 = ClapanModel.L5_DB / 1000 if ClapanModel.L5_DB is not None else None
 
 count_valves = ClapanModel.Z_valve  # Number of valves
-count_parts = sum([1 if i is not None else 0 for i in [len_part1, len_part2, len_part3, len_part4, len_part5])
+count_parts = sum([1 if i is not None else 0 for i in [len_part1, len_part2, len_part3, len_part4, len_part5]])
 Ps = [input() if i <= count_parts + 1 else None for i in range(6)]
+print(count_parts, Ps)
 proportional_coef = radius_rounding / (delta_clearance * 2)  # Proportionality coefficient
 S = delta_clearance * pi * diameter_stock  # Clearance area
 
 # Calculate inlet softening coefficient (same for all sections)
 KSI = ksi_calc(proportional_coef)
 
-''' Defining parameters by parts '''
+''' 
+Defining parameters by parts
+'''
 
+# Определение параметров Участка 1
+if len_part1:
+    if len_part2:
+        h_part1 = enthalpy_steam
+        P1_part1 = pressure_start_valve * 98066.5
+        P2_part1 = pressure_deaerator * 98066.5
+        v_part1 = steamPH(P1_part1, enthalpy_steam * 4186.8, 4)
+        t_part1 = steamPH(P1_part1, enthalpy_steam * 4186.8, 2)
+        din_vis_part1 = steamPH(P1_part1, enthalpy_steam * 4186.8, 6)
+        part_props_detection(P1_part1, P2_part1, v_part1, din_vis_part1, len_part1)
+    else:
+        # part_props_detection(P1_part1, P2_part1, v_part1, din_vis_part1, len_part1, last_part=True)
+        pass
 
-# Функция для определения параметров пара участка
-def steam_props_detection(P1, P2, v, din_vis, len_part, W=50):
-    if P1 == P2:
-        P1 += 0.03
-    kin_vis = v * din_vis
-    delta_speed = 1
-    while not (-0.001 < delta_speed < 0.001):
-        Re = (W * 2 * delta_clearance) / kin_vis
-        ALFA = 1 / (1 + KSI + (0.5 * lambda_calc(Re) * len_part) / delta_clearance) ** 0.5
-        G = ALFA * S * ((P1 ** 2 - P2 ** 2) / (P1 * v)) ** 0.5 * 3.6
-        delta_speed = W - v * G / (3.6 * S)
-        if delta_speed <= 0.001:
-            W += max(0.001, W)
-        elif delta_speed >= 0.001:
-            W -= max(0.001, W * 0.9)
-    Re = (W * 2 * delta_clearance) / kin_vis
-    ALFA = 1 / (1 + KSI + (0.5 * lambda_calc(Re) * len_part) / delta_clearance) ** 0.5
-    G = ALFA * S * ((P1 ** 2 - P2 ** 2) / (P1 * v)) ** 0.5 * 3.6
-    return G
+# Определение параметров Участка 2
+if len_part2:
+    if len_part3:
+        pass
+    else:
+        pass
 
+# Определение параметров Участка 3
+if len_part3:
+    if len_part4:
+        pass
+    else:
+        pass
 
-# Функция для определения параметров воздуха для последнего участка
-def air_props_detection(P1, P2, v, din_vis, len_part, W=50):
-    if P1 == P2:
-        P1 += 0.03
-    kin_vis = v * din_vis
-    delta_speed = 1
-    while not (-0.001 < delta_speed < 0.001):
-        Re = (W * 2 * delta_clearance) / kin_vis
-        ALFA = 1 / (1 + KSI + (0.5 * lambda_calc(Re) * len_part) / delta_clearance) ** 0.5
-        G = max(0.001, ALFA * S * ((P1 ** 2 - P2 ** 2) / (P1 * v)) ** 0.5 * 3.6)
-        delta_speed = W - v * G / (3.6 * S)
-        if delta_speed <= 0.001:
-            W += max(0.001, W)
-        elif delta_speed >= 0.001:
-            W -= max(0.001, W * 0.9)
-    Re = (W * 2 * delta_clearance) / kin_vis
-    ALFA = 1 / (1 + KSI + (0.5 * lambda_calc(Re) * len_part) / delta_clearance) ** 0.5
-    G = max(0.001, ALFA * S * ((P1 ** 2 - P2 ** 2) / (P1 * v)) ** 0.5 * 3.6)
-    return G
+# Определение параметров Участка 4
+if len_part4:
+    if len_part5:
+        pass
+    else:
+        pass
 
-
-# Определение параметров пара участка 1
-if len_part1 and len_part2:
-    # h_part1 = enthalpy_steam
-    P1_part1 = pressure_start_valve * 98066.5
-    P2_part1 = pressure_deaerator * 98066.5
-    v_part1 = steamPH(P1_part1, enthalpy_steam * 4186.8, 4)
-    # t_part1 = steamPH(P1_part1, enthalpy_steam * 4186.8, 2)
-    din_vis_part1 = steamPH(P1_part1, enthalpy_steam * 4186.8, 6)
-    steam_props_detection(P1_part1, P2_part1, v_part1, din_vis_part1, len_part1)
-else:
-    # air_props_detection()
-    pass
-
-# Определение параметров пара участка 2
-if len_part2 and len_part3:
-    # h_part2 = enthalpy_steam
-    # P1_part2 = pressure_start_valve * 98066.5
-    # P2_part2 = pressure_deaerator * 98066.5
-    # v_part2 = steamPH(P1_part2, h_part2 * 4186.8, 4)
-    # t_part2 = steamPH(P1_part2, h_part2 * 4186.8, 2)
-    # din_vis_part2 = steamPH(P1_part2, h_part2 * 4186.8, 6)
-    # steam_props_detection(P1_part2, P2_part2, v_part2, din_vis_part2, len_part2)
-    pass
-else:
-    # air_props_detection()
-    pass
-
-# Определение параметров пара участка 3
-if len_part3 and len_part4:
-    # steam_props_detection()
-    pass
-else:
-    # air_props_detection()
-    pass
-
-# Определение параметров пара участка 4
-if len_part4 and len_part5:
-    # steam_props_detection()
-    pass
-else:
-    # air_props_detection()
-    pass
-
-# Определние параметров воздуха при 5 участках
+# Определние параметров Участка 5
 if len_part5:
-    # air_props_detection()
     pass
 
 # Определение параметров пара последнего участка
@@ -195,7 +168,9 @@ if len_part5:
 # t_part3 = ClapanModel.t_vozd
 # din_vis_part3 = lambda_calc(ClapanModel.t_vozd, 2)
 
-''' Determination of parameters by suction PART '''
+''' 
+Determination of parameters by suction PART 
+'''
 
 # # Определение параметров отсоса в деаэратор
 # g_deaerator = (G_part1 - G_part2) * count_valves
@@ -224,56 +199,8 @@ if len_part5:
 # # Определение суммарного расхода воздуха
 # g_vozd = G_part3 * count_valves
 
+''' 
+Variables OUTPUT PART 
 
-''' Variables OUTPUT PART '''
-
-# ClapanModel.g_d = g_deaerator
-# ClapanModel.h_d = h_deaerator
-# ClapanModel.p_d = p_deaerator
-# ClapanModel.t_d = t_deaerator
-# ClapanModel.x_d = x_deaerator
-#
-# ClapanModel.g_e = g_ejector
-# ClapanModel.h_e = h_ejector
-# ClapanModel.p_e = p_ejector
-# ClapanModel.t_e = t_ejector
-# ClapanModel.x_e = x_ejector
-#
-# ClapanModel.g_valve = g_valve
-# ClapanModel.h_valve = h_valve
-# ClapanModel.p_valve = p_valve
-# ClapanModel.t_valve = t_valve
-#
-# ClapanModel.G_part1 = G_part1
-# ClapanModel.H_part1 = H_part1
-# ClapanModel.v_part1 = v_part1
-# ClapanModel.P1_part1 = P1_part1
-# ClapanModel.T1_part1 = T_part1
-# ClapanModel.P2_part1 = P2_part1
-# ClapanModel.Re_part1 = Re_part1
-# ClapanModel.w_part1 = _w_part1
-#
-# ClapanModel.G_part2 = G_part2
-# ClapanModel.H_part2 = H_part2
-# ClapanModel.v_part2 = v_part2
-# ClapanModel.P1_part2 = P1_part2
-# ClapanModel.T1_part2 = t_part2_
-# ClapanModel.P2_part2 = P2_part2
-# ClapanModel.Re_part2 = Re_part2
-# ClapanModel.w_part2 = _w_part2
-#
-# ClapanModel.G_part3 = G_part3
-# ClapanModel.H_part3 = H_part3
-# ClapanModel.v_part3 = v_part3
-# ClapanModel.P1_part3 = P1_part3
-# ClapanModel.T1_part3 = T_part3
-# ClapanModel.P2_part3 = P2_part3
-# ClapanModel.Re_part3 = Re_part3
-# ClapanModel.w_part3 = _w_part3
-#
-# ClapanModel.L1_Print = L1 * 1000
-# ClapanModel.L2_Print = L2 * 1000
-# ClapanModel.L3_Print = L3 * 1000
-# ClapanModel.D_Print = D * 1000
-# ClapanModel.delt_Print = delt * 1000
-# ClapanModel.f_zaz = f * 10 ** 6
+G, h, t, p... bla bla bla
+'''
