daca48af7d3a1ca29d133bf4e8266e8d1581313d
Mihanchos <shofer2007@gmail.com>
2024-09-10 22:08:00 +0500

    testAPIs add

--- backend/api/endpoints.py
+++ backend/api/endpoints.py
@@ -1,13 +1,30 @@
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+
 from backend.models.turbine import TurbineParameters
 from backend.services.calculations import calculate_efficiency
 
 router = APIRouter()
 
-@router.post("/calculate/")
-def calculate_turbine_properties(parameters: TurbineParameters):
-    try:
-        efficiency = calculate_efficiency(parameters)
-        return {"efficiency": efficiency}
-    except ZeroDivisionError:
-        raise HTTPException(status_code=400, detail="Flow rate must not be zero.")
+# @router.post("/calculate/")
+# def calculate_turbine_properties(parameters: TurbineParameters):
+#     try:
+#         efficiency = calculate_efficiency(parameters)
+#         return {"efficiency": efficiency}
+#     except ZeroDivisionError:
+#         raise HTTPException(status_code=400, detail="Flow rate must not be zero.")
+
+# Тестовый GET эндпоинт
+@router.get("/test-get")
+def test_get():
+    return {"message": "This is a test GET endpoint"}
+
+# Модель для тестового POST эндпоинта
+class TestData(BaseModel):
+    name: str
+    value: int
+
+# Тестовый POST эндпоинт
+@router.post("/test-post")
+def test_post(data: TestData):
+    return {"message": f"Received data with name: {data.name} and value: {data.value}"}
