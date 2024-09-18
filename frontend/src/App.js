7c4b670a93ff710c07d4f02e59b8fa19d2dfe375
Mihanchos <shofer2007@gmail.com>
2024-09-19 00:05:23 +0500

    react init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Калькулятор свойств турбин и штоков</h1>
+      {/* Здесь будут размещаться другие компоненты */}
+    </div>
+  );
+}
+
+export default App;
