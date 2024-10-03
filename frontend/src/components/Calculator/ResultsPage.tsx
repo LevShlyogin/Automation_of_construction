5c3fdbd89b56f0b2cc93cf23f7fc08dc1d8d32c6
Mihanchos <shofer2007@gmail.com>
2024-10-03 22:30:11 +0500

    Res page upd

--- frontend/src/components/Calculator/ResultsPage.tsx
+++ frontend/src/components/Calculator/ResultsPage.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import './ResultsPage.css'
 
 type Props = {
-  stockId: string | null;
+  stockId: string;
 };
 
 const ResultsPage: React.FC<Props> = ({ stockId }) => {
   return (
 	<div className="results-page">
-  	<h2>Результаты для штока {stockId}</h2>
-  	<p>Здесь можно добавить логику отображения результатов расчетов.</p>
+  	<h2>Шток {stockId}</h2>
+  	<p>Все параметры успешно вычислены</p>
+  	{/*
+  	<table>
+    	<thead>
+      	<tr>
+        	<th>Параметр</th>
+        	<th>Значение</th>
+      	</tr>
+    	</thead>
+    	<tbody>
+    	</tbody>
+  	</table>
+  	*/}
+  	<div className="buttons">
+    	<button className="btn-green-excel">Сохранить в виде Excel</button>
+    	<button className="btn-blue-db">Сохранить в базе данных</button>
+  	</div>
 	</div>
   );
 };
