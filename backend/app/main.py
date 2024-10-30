054acac7859b5ea7e70071145963edecf393dc09
Mihanchos <shofer2007@gmail.com>
2024-10-30 20:28:36 +0500

    CORS BULLSHIT

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,15 +22,13 @@ app = FastAPI(
     generate_unique_id_function=custom_generate_unique_id,
 )
 
-# Set all CORS enabled origins
-if settings.all_cors_origins:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["http://127.0.0.1:5173/"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
