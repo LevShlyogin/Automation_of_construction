ca2e4cc99b029c6a23c6568ed152fff819213fe6
Mihanchos <Shofer2007@gmail.com>
2024-11-23 02:07:15 +0300

    Stock and calculation connected to backend APIs

--- frontend/src/components/Calculator/StockSelection.tsx
+++ frontend/src/components/Calculator/StockSelection.tsx
@@ -1,37 +1,67 @@
 import React from 'react';
-import './StockSelection.css'; // Подключаем стили
+import './StockSelection.css';
 
-
-type Turbine = {
+type Valve = {
+  id: number;
   name: string;
-  stocks: string[];
+  type: string | null;
+  diameter: number | null;
+  clearance: number | null;
+  countParts: number | null;
+  lenPart1: number | null;
+  lenPart2: number | null;
+  lenPart3: number | null;
+  lenPart4: number | null;
+  lenPart5: number | null;
+  roundRadius: number;
+  sectionLengths: (number | null)[];
 };
-
 
 type Props = {
-  turbine: Turbine | null;
-  onSelectStock: (stock: string) => void;
+  turbine: any;
+  onSelectValve: (valve: Valve) => void;
 };
 
+const StockSelection: React.FC<Props> = ({ turbine, onSelectValve }) => {
+  const [valves, setValves] = React.useState<Valve[]>([]);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState<string | null>(null);
 
-const StockSelection: React.FC<Props> = ({ turbine, onSelectStock }) => {
-  if (!turbine) return <p>Сначала выберите турбину.</p>;
+  React.useEffect(() => {
+    const fetchValves = async () => {
+      try {
+        const response = await fetch(`http://localhost:8000/api/turbines/${turbine.name}/valves`);
+        if (!response.ok) {
+          throw new Error(`Ошибка загрузки данных. Статус: ${response.status}`);
+        }
+        const data = await response.json();
+        setValves(data.valves);
+      } catch (error: any) {
+        console.error('Ошибка запроса:', error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchValves();
+  }, [turbine]);
 
+  if (loading) return <div>Загрузка клапанов...</div>;
+  if (error) return <div>Ошибка: {error}</div>;
 
   return (
-	<div className="stock-selection">
-	<h2 className="title">{turbine.name}</h2>
-  	<h2 className="title">Выберите требуемый шток</h2>
-  	<ul className="stock-list">
-    	{turbine.stocks.map((stock, index) => (
-      	<li key={index} className="stock-item" onClick={() => onSelectStock(stock)}>
-        	{stock}
-      	</li>
-    	))}
-  	</ul>
-	</div>
+    <div className="stock-selection">
+      <h2 className="title">Выбранная турбина: {turbine.name}</h2>
+      <h3 className="title">Выберите шток для расчетов</h3>
+      <ul className="stock-list">
+        {valves.map((valve) => (
+          <li key={valve.id} className="stock-item" onClick={() => onSelectValve(valve)}>
+            <p className="stock-name">{valve.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
-
 
 export default StockSelection;
