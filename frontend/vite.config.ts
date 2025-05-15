6e52a8f5a96646713b74b976e47831f8d84fcd00
Misha-Mayskiy <Shofer2007@gmail.com>
2025-05-16 01:49:13 +0300

    feat + ref: clean from .css and refactor

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,11 @@
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
+import {TanStackRouterVite} from "@tanstack/router-vite-plugin"
 import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import {defineConfig} from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-  server: {
-      port: 3001,
-  },
+    plugins: [react(), TanStackRouterVite()],
+    server: {
+        port: 3001,
+    },
 });
