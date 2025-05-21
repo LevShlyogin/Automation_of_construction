3debc92e84f4c1c020f5fa6d630cac643d5adb09
lrshlyogin <ooooooorrrrr@gmail.com>
2025-05-21 14:03:03 +0500

    Main file refacktored

---
+++ backend/worker.py
@@ -0,0 +1,9 @@
+from backend.app.celery_app import celery_app
+
+if __name__ == "__main__":
+    celery_app.worker_main([
+        "worker",
+        "--loglevel=info",
+        "--concurrency=4",
+        "-Q", "rod",
+    ]) 
