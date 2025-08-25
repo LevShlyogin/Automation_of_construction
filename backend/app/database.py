4a6cafe0486cc830435c6ee9c6b25c201b5cfbc4
Mihanchos <shofer2007@gmail.com>
2025-08-25 15:39:38 +0500

    build: hotfix imports

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from backend.app.core.config import settings
+from app.core.config import settings
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
